f45dcb9ce2d916bd4136d0779ff0ba4294ea8d5d
kenway214 <kenway214@outlook.com>
2026-09-04 01:09:32 +0530

    custom-preferences: Fix sync issue SystemSettingPrimarySwitchPreference
    Change-Id: I77a08ac14b2e6ba1ea531e4cf41fcc90ad9c22df

--- src/com/android/settings/custom/preference/SystemSettingPrimarySwitchPreference.java
+++ src/com/android/settings/custom/preference/SystemSettingPrimarySwitchPreference.java
@@ -6,33 +6,94 @@
 package com.android.settings.custom.preference;
 
 import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
 import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
 
 import com.android.settingslib.PrimarySwitchPreference;
 
 public class SystemSettingPrimarySwitchPreference extends PrimarySwitchPreference {
 
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
+    private ContentObserver mSettingObserver;
+    private boolean mDefaultValue = false;
+
     public SystemSettingPrimarySwitchPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        setPreferenceDataStore(new DataStore());
+        init();
     }
 
     public SystemSettingPrimarySwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setPreferenceDataStore(new DataStore());
+        init();
     }
 
     public SystemSettingPrimarySwitchPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setPreferenceDataStore(new DataStore());
+        init();
     }
 
     public SystemSettingPrimarySwitchPreference(Context context) {
         super(context, null);
+        init();
+    }
+
+    private void init() {
         setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    protected void onSetInitialValue(Object defaultValue) {
+        if (defaultValue instanceof Boolean) {
+            mDefaultValue = (Boolean) defaultValue;
+        }
+        updateState();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        updateState();
+        super.onBindViewHolder(holder);
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        if (getKey() != null) {
+            if (mSettingObserver == null) {
+                mSettingObserver = new ContentObserver(mHandler) {
+                    @Override
+                    public void onChange(boolean selfChange, Uri uri) {
+                        updateState();
+                    }
+                };
+            }
+            getContext().getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(getKey()), false, mSettingObserver);
+            updateState();
+        }
+    }
+
+    @Override
+    public void onDetached() {
+        super.onDetached();
+        if (mSettingObserver != null) {
+            getContext().getContentResolver().unregisterContentObserver(mSettingObserver);
+            mSettingObserver = null;
+        }
+    }
+
+    private void updateState() {
+        if (getKey() == null) return;
+        boolean isChecked = Settings.System.getInt(
+                getContext().getContentResolver(), getKey(), mDefaultValue ? 1 : 0) != 0;
+        setChecked(isChecked);
     }
 
     private class DataStore extends PreferenceDataStore {
